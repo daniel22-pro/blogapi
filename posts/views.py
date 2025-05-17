5451e57581f932a1b53d6b219f7306d9b6717f51
Daniel Guerrero <daniguerrero012@gmail.com>
2025-05-17 10:03:29 -0500

    se remplaza generics por viewset y routers

--- posts/views.py
+++ posts/views.py
@@ -3,20 +3,15 @@ from rest_framework import generics,permissions
 from .models import Post
 from .serializers import PostSerializer, UserSerializer
 from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
-class PostList(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes =(permissions.IsAdminUser),
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-class UserList (generics.ListCreateAPIView):
+class UserViewSet (viewsets.ModelViewSet):
+    permission_classes=[IsAdminUser]
     queryset = get_user_model().objects.all()
     serializer_class=UserSerializer
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView): # new
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
