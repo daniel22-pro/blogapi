2c598578746c01f2457a7d38c2497ed80ec9fadb
Daniel Guerrero <daniguerrero012@gmail.com>
2025-05-17 08:36:57 -0500

    segundo commit

---
+++ accounts/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import AdminUserCreationForm, UserChangeForm
+from .models import CustomUser
+
+
+class CustomUserCreationForm(AdminUserCreationForm):
+	class Meta(AdminUserCreationForm):
+		model = CustomUser
+		fields = AdminUserCreationForm.Meta.fields  + ("name",)
+
+class CustomUserChangeForm(UserChangeForm):
+	class Meta:
+		model = CustomUser
+		fields = UserChangeForm.Meta.fields
--- djando_project/settings.py
+++ djando_project/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'posts.apps.PostsConfig',
 ]
+
+AUTH_USER_MODEL = "accounts.CustomUser"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
